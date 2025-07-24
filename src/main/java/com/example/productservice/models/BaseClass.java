7c2e9f7b21c85f98ef67a4c05f49b7de0471ccb7
Ramesh <ramesh200212@gmail.com>
2025-07-24 11:05:41 +0530

    created a baseclass

--- src/main/java/com/example/productservice/models/BaseClass.java
+++ src/main/java/com/example/productservice/models/BaseClass.java
@@ -3,9 +3,13 @@ package com.example.productservice.models;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 
 public abstract class BaseClass {
     private long id;
+    private Date createdAt;
+    private Date updatedAt;
 }
